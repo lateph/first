b31ee39c3d128b98b4ae39150643087c6592c731
lateph <lateph@gmail.com>
2014-08-09 00:05:48 +0700

    add api login + register

---
+++ protected/controllers/api/SiteController.php
@@ -0,0 +1,89 @@
+<?php
+
+class SiteController extends ApiController
+{
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionIndex()
+	{
+		$this->send(array('ok'));
+	}
+
+	/**
+	 * This is the action to handle external exceptions.
+	 */
+	public function actionLogin()
+	{
+		$model = new ApiLoginForm();
+		$model->attributes = $_POST;
+		if($model->validate()){
+			$member = Member::model()->find('email = :email',array(':email'=>$model->user));
+			if($member === null)
+				$member = Member::model()->find('username = :user',array(':user'=>$model->user));
+			if($member === null)
+				$this->sendErrorMessage('User Not Found');
+			if($member->validatePassword($model->password)){
+				$member->generateToken();
+				$this->send(array(
+					'token'=>$member->token,
+				));
+			}
+			else{
+				$this->sendErrorMessage('Wrong User or password');
+			}
+		}else{
+			$this->send($model->getErrors(),0);
+		}
+		
+	}
+
+	public function actionRegister(){
+		$model = new ApiRegisterForm();
+		$model->attributes = $_POST;
+		if($model->save()){
+			$model->generateToken();
+			$this->send(array(
+				'token'=>$model->token,
+			));
+		}else{
+			$this->send($model->getErrors(),0);
+		}
+	}
+
+	/**
+	 * Displays the contact page
+	 */
+	public function actionContact()
+	{
+		$model=new ContactForm;
+		if(isset($_POST['ContactForm']))
+		{
+			$model->attributes=$_POST['ContactForm'];
+			if($model->validate())
+			{
+				$name='=?UTF-8?B?'.base64_encode($model->name).'?=';
+				$subject='=?UTF-8?B?'.base64_encode($model->subject).'?=';
+				$headers="From: $name <{$model->email}>\r\n".
+					"Reply-To: {$model->email}\r\n".
+					"MIME-Version: 1.0\r\n".
+					"Content-type: text/plain; charset=UTF-8";
+
+				mail(Yii::app()->params['adminEmail'],$subject,$model->body,$headers);
+				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
+				$this->refresh();
+			}
+		}
+		$this->render('contact',array('model'=>$model));
+	}
+
+	/**
+	 * Logs out the current user and redirect to homepage.
+	 */
+	public function actionLogout()
+	{
+		Yii::app()->user->logout();
+		$this->redirect(Yii::app()->homeUrl);
+	}
+}
---
+++ protected/models/api/ApiRegisterForm.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * ContactForm class.
+ * ContactForm is the data structure for keeping
+ * contact form data. It is used by the 'contact' action of 'SiteController'.
+ */
+class ApiRegisterForm extends Member
+{
+	public $passwordKonfirm;
+
+	/**
+	 * Declares the validation rules.
+	 */
+	public function rules()
+	{
+		return array(
+			// email has to be a valid email address
+			array('username,email,password,passwordKonfirm', 'required'),
+			array('username,email','unique'),
+
+			array('passwordKonfirm', 'compare', 'compareAttribute'=>'password'),
+			// verifyCode needs to be entered correctly
+		);
+	}
+
+	/**
+	 * Declares customized attribute labels.
+	 * If not declared here, an attribute would have a label that is
+	 * the same as its name with the first letter in upper case.
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'verifyCode'=>'Verification Code',
+		);
+	}
+}
